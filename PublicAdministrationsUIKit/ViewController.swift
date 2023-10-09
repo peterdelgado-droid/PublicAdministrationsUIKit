c99039ba5d597aa4d6670be8d90e34c3375eb147
Peter Delgado <peterrafaeldelgado@gmail.com>
2023-10-09 17:27:54 +0200

    Initial Commit

---
+++ PublicAdministrationsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PublicAdministrationsUIKit
+//
+//  Created by Peter Delgado on 9/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
